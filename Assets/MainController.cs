7f8eabe09349fff40c4781b8f95f6823b8fe17e6
ajul <ajul1987@gmail.com>
2018-02-01 03:36:17 -0500

    begin code

---
+++ Assets/MainController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour {
+
+    public Camera shipRenderCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        shipRenderCamera.Render();
+    }
+}
---
+++ Assets/ShipRenderCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipRenderCamera : MonoBehaviour {
+
+    public Material hullMaterial;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnPostRender() {
+
+        hullMaterial.SetPass(0);
+
+        GL.Begin(GL.TRIANGLES);
+
+        GL.Color(new Color(1.0f, 0.0f, 0.0f, 2.0f));
+        GL.Vertex3(0.0f, 0.0f, 1.0f);
+
+        GL.Color(new Color(0.0f, 1.0f, 0.0f, 2.0f));
+        GL.Vertex3(50.0f, 0.0f, 1.0f);
+
+        GL.Color(new Color(0.0f, 0.0f, 1.0f, 2.0f));
+        GL.Vertex3(0.0f, 50.0f, 1.0f);
+
+        GL.End();
+
+        
+    }
+}
